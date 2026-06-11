fb8a19191516e5a7fd485ac4a3ea447187657b07
Vinodk999 <kumarkvinod723@gmail.com>
2026-06-11 20:29:40 +0530

    Updated main.tf file

--- project-2/main.tf
+++ project-2/main.tf
@@ -11,7 +11,7 @@ provide "aws" {
     region = "us-east-1"
 }
 
-resoutce "aws_vpc" "my-vpc" {
+resource "aws_vpc" "my-vpc" {
     cidr_block = "10.0.0.0/16"
     enable_dns_support = true
     enable_dns_hostnames = true
